2a6ea1b2d150bf822128b92c540fc2f6f7f3bbd2
darkfos82 <darkfos82@gmail.com>
2025-09-28 18:16:07 +0300

    update: Обновил конфигурацию Vite для деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import {AntDesignVueResolver} from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/about/',
   plugins: [
     vue(),
     vueJsx(),
